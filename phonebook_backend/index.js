b0559a0ee507222c2ef97a000dcc78b0ea0e95fd
mrance21 <mrance21@gmail.com>
2022-12-20 17:47:40 -0500

    added deletion feature for DB

--- phonebook_backend/index.js
+++ phonebook_backend/index.js
@@ -65,10 +65,26 @@ app.get('/api/persons/:id', (req, res) => {
   }
 })
 
-app.delete('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id)
-  persons = persons.filter(person => person.id !== id)
-  res.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+        response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+
+  const person = {
+      number: body.number 
+  }
+
+  Person.findById(request.params.id, person, { new: true })
+      .then(updatedPerson => {
+          response.json(updatedPerson)
+      })
+      .catch(error => next(error))
 })
 
 app.post('/api/persons', (req, res) => {
@@ -79,11 +95,12 @@ app.post('/api/persons', (req, res) => {
       error: "name or number is missing"
     })
   } 
-  // if (persons.find(person => person.name === body.name)) {
-  //   return res.status(400).json({
-  //     error: "name must be unique"
-  //   })
-  // }
+
+  if (persons.find(person => person.name === body.name)) {
+    return res.status(400).json({
+      error: "name must be unique"
+    })
+  }
 
   const person = new Person({
     name: body.name,
@@ -93,6 +110,7 @@ app.post('/api/persons', (req, res) => {
   person.save().then(savedPerson => {
     res.json(person)
   })
+  .catch(error => next(error))
 })
 
 // #middleware after our routes, that is used for catching 
@@ -102,6 +120,18 @@ const unknownEndpoint = (req, res) => {
 }
 
 app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 PORT = process.env.PORT
 app.listen(PORT, () => {
